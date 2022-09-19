7309c6bdf9590e07b684a792f26c238cc2edfd2d
imDatt777 <ashutoshdatt777@gmail.com>
2022-09-19 23:26:27 +0530

    Add a note and fetch notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,53 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
-router.get('/', (req, res)=>{
+// Importing middleware
+const fetchUser = require("../middleware/fetchUser");
 
-    res.json([])
-} )
+// Importing Schema
+const Note = require("../models/Note");
 
-module.exports = router
+// *********************************************ROUTE-1*********************************************
+// Fetch all notes using GET '/api/notes/fetchallnotes'. Login required
+router.get("/fetchallnotes", fetchUser, async (req, res) => {
+  try {
+    const notes = await Note.find({ user: req.user.id });
+    res.json(notes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Something went wrong!");
+  }
+});
+
+module.exports = router;
+
+// *********************************************ROUTE-2*********************************************
+// Add a note using POST '/api/notes/addnote'. Login required
+router.post(
+  "/addnote",
+  fetchUser,
+  [
+    body("title", "Title is mandatory").isLength({ min: 1 }),
+    body("description", "Description is mandatory").isLength({ min: 1 }),
+    body("tag", "Tag is mandatory").isLength({ min: 1 }),
+  ],
+  async (req, res) => {
+    try {
+      // If there are errors, return bad request with errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      //   Creating a new note
+      const { title, description, tag } = req.body;
+      const note = new Note({ title, description, tag, user: req.user.id });
+      const savedNote = await note.save();
+      res.json(savedNote);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Something went wrong!");
+    }
+  }
+);
